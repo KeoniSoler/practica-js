4a50eb21e394c0186d4f0d542e72c11881295e7d
KeoniSoler <keoni@Keonis-MacBook-Pro.local>
2024-05-31 09:43:31 -0300

    Reemplazando por el zip q mandaron como base

--- js/rickAndMorty.js
+++ js/rickAndMorty.js
@@ -1,28 +1,31 @@
-let url = "https://rickandmortyapi.com/api/character";
+let url = 'https://rickandmortyapi.com/api/character';
 
 fetch(url)
-    .then(function(response) {
-        return response.json()
-      })
-      .then(function(data) {
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(data){
+        //Acá ya tenemmos los datos finales y es donde debemos escribir nuestro código.
         console.log(data);
-        const characterList = document.querySelector('.characterList');
-        let characters = '';
+        let arrayDePersonajes = data.results;
 
-        for (let i = 0; i < data.results.length; i++) {
-            const character = data.results[i];
+        //1 Donde: Capturo el elemento html en donde quiero hacer una modificación
+        let seccion = document.querySelector('.container');
+        let allCharacters = [];
 
-            characters += `
-            <article>
-                <img src="${character.image}" alt="${character.name}">
-                <p>Name: ${character.name}</p>
-                <p>Status: ${character.status}</p>
-            </article>
-        `;
+        //2 Qué: recorro la información de la api y la organizo para mostarla en el html
+        for(let i=0; i<arrayDePersonajes.length; i++){
+            //Dentro del for voy acumulando en la variable una estructura html por cada personaje del array.
+            allCharacters += `<article>
+                                <img src=${arrayDePersonajes[i].image} alt='${arrayDePersonajes[i].name}' />
+                                <p>Name: ${arrayDePersonajes[i].name} </p>
+                                <p>Status: ${arrayDePersonajes[i].status} </p>
+                            </article>`
         }
-        characterList.innerHTML = characters;
+        //Con toda la estructura html completa ahora la paso al DOM
+        seccion.innerHTML = allCharacters;
 
-      })
-      .catch(function(error) {
-        console.log("Error: " + error);  
-});
+    })
+    .catch( function(e){
+        console.log(e)
+    })
